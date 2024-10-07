2262a033ebba11f9a5e7cec8213df0989027701c
Akmoor <160403007akuwa@gmail.com>
2024-10-07 11:49:20 +0200

    Login Test

--- src/main/java/core/BasePage.java
+++ src/main/java/core/BasePage.java
@@ -10,7 +10,7 @@ public abstract class BasePage {
     public JavascriptExecutor js;
     public BasePage(WebDriver driver){
         this.driver=driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver,this); //POM without locators
         js=(JavascriptExecutor) driver;
     }
     public void click(WebElement element){
@@ -20,11 +20,29 @@ public abstract class BasePage {
         //moveToView(element);
         click(element);
     }
+    public void  clickWithJS(WebElement element, int a, int b){
+        js.executeScript("window.scrollBy "+a+", "+b);
+        click(element);
+    }
+    public void moveToView(WebElement element) {
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
     public void type(WebElement element,String text){
         if (text != null) {
             click(element);
             element.clear();
             element.sendKeys(text);
         }
+    }
+    public void pause(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    public void hideAds() {
+        js.executeScript("");
+        js.executeScript("");
     }
 }
